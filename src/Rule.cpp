1e777d7c0d67abc7b1a516d1a3b639d85d7c2557
Alexandros Naskos <alex_naskos@hotmail.com>
2012-02-21 20:52:42 +0200

    Removed useless files

--- src/Rule.cpp
+++
@@ -1,5 +0,0 @@
-#include "Rule.h"
-using namespace BPS;
-
-
-Rule::Rule(Symbol* direction,Symbol* speed,Symbol* acceleration,Symbol* lifetime,Symbol* spawnedNumber,Value* _condition,List* _effect) : directionSym(direction), speedSym(speed), accelerationSym(acceleration), lifetimeSym(lifetime), spawnedNumberSym(spawnedNumberSym),consition(_condition), effect(_effect) {}
